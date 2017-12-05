12b7b87d58683dd13f4d4e7b97137d9807a979e8
AyumiizZ <korrawitc@gmail.com>
2017-12-05 17:02:31 +0700

    add FreeNode

--- Linked_List.c
+++ Linked_List.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 typedef struct Node
 {
     char Info;
     struct Node *Next;
 } TYPE_NODE;
 typedef struct Node *TYPE_NODEPTR;
-TYPE_NODEPTR NewNode(char Item){
+TYPE_NODEPTR NewNode(char Item)
+{
     TYPE_NODEPTR N;
-    N = (TYPE_NODEPTR) malloc(sizeof(TYPE_NODE));
-    N -> Info = Item;
-    N -> Next = NULL;
+    N = (TYPE_NODEPTR)malloc(sizeof(TYPE_NODE));
+    N->Info = Item;
+    N->Next = NULL;
     return N;
 }
-
+void FreeNode(TYPE_NODEPTR N)
+{
+    free(N);
+}
 int main()
 {
     printf("Hello World");
